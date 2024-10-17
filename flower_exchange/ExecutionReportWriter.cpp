035e9ce643c96cee3f295251b570acb2e5661d20
Navini Jagoda <123384639+Navini11@users.noreply.github.com>
2024-10-17 21:29:16 +0530

    Update ExecutionReportWriter.cpp

--- flower_exchange/ExecutionReportWriter.cpp
+++ flower_exchange/ExecutionReportWriter.cpp
@@ -1,17 +1,1 @@
-// ExecutionReportWriter
 
-#include "ExecutionReportWriter.h"
-#include <fstream>
-#include <iostream>
-
-ExecutionReportWriter::ExecutionReportWriter(const std::string& filename) : filename(filename) {
-}
-
-void ExecutionReportWriter::writeReport(const std::string& report) {
-    std::ofstream file(filename, std::ios::app);
-    if (file.is_open()) {
-        file << report << std::endl;
-    } else {
-        std::cerr << "Unable to open file: " << filename << std::endl;
-    }
-}
