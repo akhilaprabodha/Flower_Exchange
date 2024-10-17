04ed7272e3c8f76ec1bd09b8e996cb3edc9fd18c
Navini Jagoda <123384639+Navini11@users.noreply.github.com>
2024-10-17 21:40:05 +0530

    Update OrderSplitter.cpp

--- flower_exchange/OrderSplitter.cpp
+++ flower_exchange/OrderSplitter.cpp
@@ -1,14 +1,1 @@
-// OrderSplitter
-
 #include "OrderSplitter.h"
-#include <sstream>
-
-std::vector<std::string> OrderSplitter::splitOrder(const std::string& order) {
-    std::vector<std::string> result;
-    std::stringstream ss(order);
-    std::string item;
-    while (std::getline(ss, item, ',')) {
-        result.push_back(item);
-    }
-    return result;
-}
